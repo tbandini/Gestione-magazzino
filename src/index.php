8e8bfb4bfa98c2e7005f957ff8d769b5e1bb47b5
tbandini <tommasobandini6@gmail.com>
2023-01-23 23:57:48 +0100

    Aggiornata la pagina di inserimento nuova categoria2

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-include("devs.php");
+include("fetch-data.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,7 +7,7 @@ include("devs.php");
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Tabella con tecnica di reload</title>
+        <title>Gestione magazzino</title>
         <link rel="stylesheet" href="style.css" />
         <script src="index.js" defer></script>
     </head>
--- src/nuova-categoria.php
+++ src/nuova-categoria.php
@@ -19,12 +19,37 @@ if($rs)
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Gestione magazzino</title>
+	<link rel="stylesheet" href="style.css" />
+	<script src="index.js" defer></script>
 </head>
 <body>
-	
+	<header>
+		<h1>Gestione magazzino</h1>
+		<h2>inserimento-categorie</h2>
+	</header>
+	<form form method="POST" id="queryForm">
+		<label for="Codice">Codice:</label>
+		<input type="text" name="Codice" id="Codice">
+
+		<label for="Nome">Nome:</label>
+		<input type="text" name="Nome" id="Nome">
+
+		<label for="Descrizione">Descrizione:</label>
+		<input type="text" name="Descrizione" id="Descrizione">
+
+		<label for="radioSelect">Attivo:</label>
+		<div name="radioSelect">
+			<input type="radio" name="attivo" id="Attivo" value="1">
+			<p for="Attivo">Si</p>
+			<input type="radio" name="attivo" id="nonAttivo" value="0">
+			<p for="nonAttivo">No</p>
+		</div>
+
+		<button id="btn" type="submit">Invia</button>
+	</form>
 </body>
 </html>
