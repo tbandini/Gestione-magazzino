e1d1580daa1969bed786a28a553327b2f9707241
tbandini <tommasobandini6@gmail.com>
2023-02-06 11:09:36 +0100

    fine commit precedente

--- src/query-categoria.php
+++ src/query-categoria.php
@@ -7,7 +7,17 @@ $Nome = $_POST['Nome'];
 $Descrizione = $_POST['Descrizione'];
 $Attivo = $_POST['Attivo'];
 
-$query = "INSERT INTO `Categorie` (`ID`, `Codice`, `Nome`, `Descrizione`, `Attivo`) VALUES (null, '$Codice', '$Nome', '$Descrizione', '$Attivo');";
+// Query per inserire una nuova categoria che crea a sua volta una tabella collegata a questa categoria
+$query = "INSERT INTO `Categorie` (`ID`, `Codice`, `Nome`, `Descrizione`, `Attivo`) VALUES (null, '$Codice', '$Nome', '$Descrizione', '$Attivo');
+		CREATE TABLE `$Nome` (
+			`ID` int(11) NOT NULL AUTO_INCREMENT,
+			`Codice` varchar(255) NOT NULL,
+			`Nome` varchar(255) NOT NULL,
+			`Descrizione` varchar(255) NOT NULL,
+			`Attivo` varchar(255) NOT NULL,
+			PRIMARY KEY (`ID`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+";
 
 $rs = mysqli_query($con, $query);
 
